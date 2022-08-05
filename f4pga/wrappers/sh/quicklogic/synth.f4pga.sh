376c15dd21a620d0a39a2dc8fb9b5875e21559d3
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-06 00:23:59 +0200

    f4pga/wrappers/sh/synth: style
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- f4pga/wrappers/sh/quicklogic/synth.f4pga.sh
+++ f4pga/wrappers/sh/quicklogic/synth.f4pga.sh
@@ -18,22 +18,6 @@
 
 set -e
 
-print_usage () {
-    echo "Usage: symbiflow_synth  -v|--verilog <Verilog file list>"
-    echo "                       [-t|--top <top module name>]"
-    echo "                       [-F|--family <device family>]"
-    echo "                       [-d|--device <device type (e.g. qlf_k4n8)>]"
-    echo "                       [-P|--part <part name>]"
-    echo "                       [-p|--pcf <PCF IO constraints>]"
-    echo "                       [-y <Verilog library search path>"
-    echo "                       [+libext+<Verilog library file extension>]"
-    echo "                       [+incdir+<Verilog include path>]"
-    echo "                       [+define+<macro name>[=<macro value>]]"
-    echo "                       [-f <additional compile command file>]"
-    echo ""
-    exit 1
-}
-
 VERILOG_FILES=()
 TOP="top"
 DEVICE=""
@@ -45,8 +29,8 @@ EXTRA_ARGS=()
 OPT=""
 for arg in $@; do
   case $arg in
-    -t|--top)     OPT="top" ;;
     -v|--verilog) OPT="vlog" ;;
+    -t|--top)     OPT="top" ;;
     -d|--device)  OPT="dev" ;;
     -F|--family)  OPT="family" ;;
     -P|--part)    OPT="part" ;;
@@ -54,58 +38,68 @@ for arg in $@; do
     -y|-f|+incdir+*|+libext+*|+define+*) OPT="xtra" ;;
     *)
       case $OPT in
-        "top")    TOP=$arg;    OPT="" ;;
-        "dev")    DEVICE=$arg; OPT="" ;;
-        "family") FAMILY=$arg; OPT="" ;;
-        "part")   PART=$arg;   OPT="" ;;
-        "pcf")    PCF=$arg;    OPT="" ;;
         "vlog")   VERILOG_FILES+=($arg) ;;
+        "top")    TOP=$arg    OPT="" ;;
+        "dev")    DEVICE=$arg OPT="" ;;
+        "family") FAMILY=$arg OPT="" ;;
+        "part")   PART=$arg   OPT="" ;;
+        "pcf")    PCF=$arg    OPT="" ;;
         "xtra") ;;
-        *) print_usage ;;
+        *)
+          echo "
+Usage: symbiflow_synth  -v|--verilog <Verilog file list>
+                       [-t|--top <top module name>]
+                       [-F|--family <device family>]
+                       [-d|--device <device type (e.g. qlf_k4n8)>]
+                       [-P|--part <part name>]
+                       [-p|--pcf <PCF IO constraints>]
+                       [-y <Verilog library search path>
+                       [+libext+<Verilog library file extension>]
+                       [+incdir+<Verilog include path>]
+                       [+define+<macro name>[=<macro value>]]
+                       [-f <additional compile command file>]
+"
+          exit 1
+        ;;
       esac
       ;;
   esac
   if [ "$OPT" == "xtra" ]; then EXTRA_ARGS+=($arg); fi
 done
 
-if [ -z ${FAMILY} ]; then echo "Please specify device family"; exit 1; fi
-if [ ${#VERILOG_FILES[@]} -eq 0 ]; then echo "Please provide at least one Verilog file"; exit 1; fi
+if [ -z "${FAMILY}" ]; then echo "Please specify device family"; exit 1; fi
+if [ "${#VERILOG_FILES[@]}" -eq 0 ]; then echo "Please provide at least one Verilog file"; exit 1; fi
 
-PINMAPCSV="pinmap_${PART}.csv"
+export USE_ROI='FALSE'
+export OUT_JSON="$TOP".json
+export SYNTH_JSON="${TOP}"_io.json
+export OUT_SYNTH_V="${TOP}"_synth.v
+export OUT_EBLIF="${TOP}".eblif
+export OUT_FASM_EXTRA="${TOP}"_fasm_extra.fasm
+export PYTHON3="${PYTHON3:=$(which python3)}"
+export UTILS_PATH="${F4PGA_SHARE_DIR}"/scripts
+export TECHMAP_PATH="${F4PGA_SHARE_DIR}/techmaps/${FAMILY}"
 
-export USE_ROI="FALSE"
-export OUT_JSON=$TOP.json
-export SYNTH_JSON=${TOP}_io.json
-export OUT_SYNTH_V=${TOP}_synth.v
-export OUT_EBLIF=${TOP}.eblif
-export OUT_FASM_EXTRA=${TOP}_fasm_extra.fasm
-export PYTHON3=$(which python3)
-
-if [ -s $PCF ]; then
-  export PCF_FILE=$PCF
-else
-  export PCF_FILE=""
-fi
+export PCF_FILE=""
+if [ -s "$PCF" ]; then export PCF_FILE=$PCF; fi
 
 DEVICE_PATH="${F4PGA_SHARE_DIR}/arch/${DEVICE}_${DEVICE}"
-export PINMAP_FILE=${DEVICE_PATH}/${PINMAPCSV}
-if [ -d "${DEVICE_PATH}/cells" ]; then
-  export DEVICE_CELLS_SIM=`find ${DEVICE_PATH}/cells -name "*_sim.v"`
-  export DEVICE_CELLS_MAP=`find ${DEVICE_PATH}/cells -name "*_map.v"`
-else
+
+if [ ! -d "${DEVICE_PATH}/cells" ]; then
   # pp3 family has different directory naming scheme
   # the are named as ${DEVICE}_${PACKAGE}
   # ${PACKAGE} is not known because it is not passed down in add_binary_toolchain_test
   DEVICE_PATH=$(find "${F4PGA_SHARE_DIR}"/arch/ -type d -name "${DEVICE}*")
-  export PINMAP_FILE=${DEVICE_PATH}/${PINMAPCSV}
-  if [ -d "${DEVICE_PATH}/cells" ]; then
-    export DEVICE_CELLS_SIM=`find ${DEVICE_PATH}/cells -name "*_sim.v"`
-    export DEVICE_CELLS_MAP=`find ${DEVICE_PATH}/cells -name "*_map.v"`
-  else
-    export DEVICE_CELLS_SIM=
-    export DEVICE_CELLS_MAP=
-  fi
 fi
+
+export DEVICE_CELLS_SIM=
+export DEVICE_CELLS_MAP=
+if [ -d "${DEVICE_PATH}/cells" ]; then
+  export DEVICE_CELLS_SIM=`find ${DEVICE_PATH}/cells -name "*_sim.v"`
+  export DEVICE_CELLS_MAP=`find ${DEVICE_PATH}/cells -name "*_map.v"`
+fi
+
+export PINMAP_FILE="${DEVICE_PATH}/pinmap_${PART}.csv"
 
 yosys_cmds=`echo ${EXTRA_ARGS[*]} | python3 -m f4pga.utils.quicklogic.convert_compile_opts`
 if [ ! -z "${yosys_cmds}" ]; then yosys_cmds="${yosys_cmds//$'\n'/'; '}; "; fi
